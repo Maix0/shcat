dfb10f33905972179d390a433b0ca75329099b87
Maieul BOYER <maieul.dev@familleboyer.net>
2024-04-29 14:04:02 +0200

    Fix: Makefile does  compile correctly now

--- Minishell.mk
+++ Minishell.mk
@@ -6,7 +6,7 @@
 #    By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/28 17:28:30 by maiboyer          #+#    #+#              #
-#    Updated: 2024/04/29 14:00:12 by maiboyer         ###   ########.fr        #
+#    Updated: 2024/04/29 14:02:41 by maiboyer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@
 OBJDIRNAME ?=
 
 # Flags
-CFLAGS = -Werror -Wextra -Wall -Wno-unused-command-line-argument -g3 -MMD -lreadline  -I./includes
+CFLAGS = -Werror -Wextra -Wall -Wno-unused-command-line-argument -g3 -MMD -lreadline  -I./includes -I./output/include
 # Sources
 LIB =	./libft/ft_bzero.c \
 		./libft/ft_calloc.c \
