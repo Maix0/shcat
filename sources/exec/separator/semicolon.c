771deeb02ab64dfacf7e8b691a6a8df508d11b78
EniumRaphael <rparodi@student.42.fr>
2024-05-12 16:12:43 +0200

    Starting to work on the differents semicolon

---
+++ sources/exec/separator/semicolon.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   semicolon.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 16:00:33 by rparodi           #+#    #+#             */
+/*   Updated: 2024/05/12 16:03:31 by rparodi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "app/env.h"
+#include "app/state.h"
+#include "me/types.h"
+#include "gmr/symbols.h"
+#include "app/node.h"
+#include "me/vec/vec_str.h"
+// #include "app/node/handle_program.h"
+#include "app/node/handle_command.h"
+#include "minishell.h"
+#include "me/string/str_clone.h"
+
+t_error semicolon_exec(t_node *first, t_node *second, t_i32 ret_value)
+{
+	return (NO_ERROR);
+}
