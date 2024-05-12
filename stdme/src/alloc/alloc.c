f2256d0f58d6f72be5ba2601403ba8eb2ca08671
Maix0 <maix522@gmail.com>
2024-05-12 22:16:20 +0200

    ...

--- stdme/src/alloc/alloc.c
+++ stdme/src/alloc/alloc.c
@@ -6,7 +6,7 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 10:13:06 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/05/12 14:05:48 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/05/10 21:39:07 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void __libc_free(void *ptr);
-
 void *me_malloc(t_usize size)
 {
 	t_mblock *block;
-	void	 *ret;
 
 	size = usize_round_up_to(size, 16);
-	printf("Allocating %zu.\n", size);
 	block = get_block_for_size(size);
 	if (block == NULL)
 		return (me_abort("Found no page for me_malloc"), NULL);
 	block->used = true;
-	ret = ((t_u8 *)block->page->data) + block->offset;
-	mem_set_zero(ret, block->size);
-	return (ret);
+	mem_set_zero((t_u8 *)block + sizeof(*block), block->size);
+	return ((void *)(((t_usize)block) + sizeof(t_mblock)));
 }
 
 void *me_calloc(t_usize elem_size, t_usize elem_count)
@@ -54,11 +49,17 @@ void *me_realloc(void *ptr, t_usize new_size)
 
 	if (ptr == NULL)
 		return (me_malloc(new_size));
-	block = get_block_from_ptr(ptr);
-	if (block == NULL || block->size <= new_size)
+	block = (void *)((t_u8 *)(ptr) - sizeof(t_mblock));
+	if (block->size <= new_size)
 		return (ptr);
-	if (!merge_next_block(block, new_size))
+	if (block->next && block->next->page == block->page && !block->used &&
+		block->next->size + block->size + sizeof(t_mblock) >= new_size)
+
+	{
+		block->size = block->size + block->next->size + sizeof(t_mblock);
+		block->next = block->next->next;
 		return (ptr);
+	}
 	else
 	{
 		ret = me_malloc(new_size);
@@ -72,11 +73,12 @@ void me_free(void *ptr)
 {
 	t_mblock *cur;
 
-	if (ptr == NULL)
-		return;
-	cur = get_block_from_ptr(ptr);
-	if (cur == NULL)
-		return (me_abort("Invalid free (not allocated with me_*alloc)!"));
+	cur = (void *)(((t_usize)ptr) - sizeof(t_mblock));
 	cur->used = false;
-	merge_next_block(cur, ~0);
+	if (cur->next != NULL && cur->page == cur->next->page && !cur->next->used)
+	{
+		cur->size += sizeof(t_mblock) + cur->next->size;
+		mem_set_zero(cur->next->padding, 7);
+		cur->next = cur->next->next;
+	}
 }
--- stdme/src/alloc/get_arena.c
+++ stdme/src/alloc/get_arena.c
@@ -6,7 +6,7 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 09:47:50 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/05/12 14:03:55 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/05/09 18:21:33 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,49 +23,34 @@
 #include <stdlib.h>
 
 void *__libc_malloc(size_t size);
-void  __libc_free(void *ptr);
-
-void free_ifn(void *ptr)
-{
-	if (ptr != NULL)
-		__libc_free(ptr);
-}
 
 t_mpage *alloc_page(t_usize size)
 {
-	t_mpage	 *page;
-	void	 *data;
-	t_mblock *block;
+	t_mpage *val;
 
-	size = usize_round_up_to(size, PAGE_SIZE_DEFAULT);
-	page = __libc_malloc(sizeof(t_mpage));
-	block = __libc_malloc(sizeof(t_mblock));
-	data = __libc_malloc(size);
-	if (page == NULL || data == NULL || block == NULL || PAGE_SIZE_DEFAULT <= 0)
-		return (free_ifn(page), free_ifn(data), free_ifn(block), NULL);
-	page->data = data;
-	page->next = NULL;
-	page->page_size = size;
-	page->first = block;
-	block->offset = 0;
-	block->page = page;
-	block->next = NULL;
-	block->used = false;
-	block->size = size;
-	mem_copy(block->padding, BLOCK_PADDING, 7);
-	return (page);
+	size = usize_round_up_to(size + sizeof(t_mpage), PAGE_SIZE_DEFAULT);
+	val = __libc_malloc(size);
+	if (val == NULL || sizeof(t_mpage)  + sizeof(t_mblock) >= PAGE_SIZE_DEFAULT)
+		return (NULL);
+	val->next = NULL;
+	val->page_size = size;
+	val->first = (t_mblock *)(((t_usize)val) + sizeof(t_mpage));
+	val->first->page = val;
+	val->first->next = NULL;
+	mem_copy(val->first->padding, BLOCK_PADDING, 7);
+	val->first->used = false;
+	val->first->size = size - sizeof(t_mblock) - sizeof(t_mpage);
+	return (val);
 }
 
 t_mpage *get_head_arena(void)
 {
 	static t_mpage *val = NULL;
 
+	if (val == NULL && PAGE_SIZE_DEFAULT > sizeof(t_mpage))
+		val = alloc_page(PAGE_SIZE_DEFAULT - sizeof(t_mpage));
 	if (val == NULL)
-	{
-		val = alloc_page(PAGE_SIZE_DEFAULT);
-		if (val == NULL)
-			(me_putstr_fd("Failed to alloc first page", 2), exit(1));
-	}
+		(me_putstr_fd("Failed to alloc first page", 2), exit(1));
 	return (val);
 }
 
@@ -73,26 +58,23 @@ t_mblock *split_block(t_mblock *self, t_usize min_size)
 {
 	t_usize	  remaining;
 	t_mblock *old_next;
-	t_mblock *new_next;
 
 	min_size = usize_round_up_to(min_size, 16);
-	if (self->size > min_size)
+	if (self->size > (min_size + sizeof(t_mblock) + 16))
 	{
-		remaining = self->size - min_size;
-		new_next = __libc_malloc(sizeof(t_mblock));
-		if (new_next == NULL)
-			return (me_abort("Failed to alloc block"), NULL);
-		printf("splitting %zu into %zu and %zu\n", self->size, min_size,
-			   remaining);
+		remaining = self->size - min_size - sizeof(t_mblock);
+		printf("splitting %zu into %zu and %zu\n", self->size, min_size, remaining);
+		if (self->size == 80 && min_size == 16 && remaining == 32)
+			printf("HERE\n");
 		self->size = min_size;
 		old_next = self->next;
-		new_next->page = self->page;
-		new_next->next = old_next;
-		new_next->offset = self->offset + self->size;
-		new_next->used = false;
-		new_next->size = remaining;
-		mem_copy(new_next->padding, BLOCK_PADDING, 7);
-		self->next = new_next;
+		self->next =
+			(t_mblock *)(((t_usize)self) + self->size + sizeof(t_mblock));
+		self->next->page = self->page;
+		self->next->next = old_next;
+		self->next->used = false;
+		self->next->size = remaining;
+		mem_copy(self->next->padding, BLOCK_PADDING, 7);
 	}
 	return (self);
 }
@@ -104,11 +86,8 @@ t_mblock *get_block_for_size(t_usize size)
 
 	last = NULL;
 	cur = get_head_arena()->first;
-	printf("cur == %p\n", cur);
 	while (cur)
 	{
-		if (cur->page == NULL)
-			me_abort("block doesn't have a page ?????");
 		if (!cur->used && cur->size >= size)
 			return (split_block(cur, size));
 		last = cur;
@@ -116,44 +95,11 @@ t_mblock *get_block_for_size(t_usize size)
 	}
 	if (last == NULL)
 		return (NULL);
-	last->page->next = alloc_page(size);
+	last->page->next = alloc_page(size + sizeof(t_mblock));
 	if (last->page->next == NULL)
 		me_abort("Failed to alloc page!");
 	last->next = last->page->next->first;
 	return (split_block(last->page->next->first, size));
-}
-
-t_mblock *get_block_from_ptr(void *ptr)
-{
-	t_mpage	 *page;
-	t_mblock *block;
-
-	page = get_page_from_ptr(ptr);
-	if (page == NULL)
-		return (NULL);
-	block = page->first;
-	while (block && block->page == page)
-	{
-		if ((t_u8 *)page->data + block->offset == (t_u8 *)ptr)
-			return (block);
-		block = block->next;
-	}
-	return (NULL);
-}
-
-t_mpage *get_page_from_ptr(void *ptr)
-{
-	t_mpage *page;
-
-	page = get_head_arena();
-	while (page)
-	{
-		if ((t_u8 *)page->data <= (t_u8 *)ptr &&
-			(t_u8 *)ptr < ((t_u8 *)page->data + page->page_size))
-			return (page);
-		page = page->next;
-	}
-	return (NULL);
 }
 
 void print_pages_info(void)
@@ -172,21 +118,4 @@ void print_pages_info(void)
 		me_putstr_fd("\n", 2);
 		page = page->next;
 	}
-}
-
-t_error merge_next_block(t_mblock *cur, t_usize min_size)
-{
-	t_mblock *next;
-
-	if (cur->next != NULL && cur->page == cur->next->page && !cur->next->used &&
-		cur->size + cur->next->size >= min_size)
-	{
-		cur->size += cur->next->size;
-		next = cur->next;
-		cur->next = cur->next->next;
-		printf("merging two blocks %p and %p\n", cur, cur->next);
-		__libc_free(next);
-		return (NO_ERROR);
-	}
-	return (ERROR);
 }
