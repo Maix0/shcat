05b1ddda6fa6c615e2c592cccb0027d7f627c2ca
Maix0 <maix522@gmail.com>
2024-05-16 00:10:45 +0200

    wip

---
+++ allocator/include/aq/melloc_interal.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   melloc_interal.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 15:27:46 by maiboyer          #+#    #+#             */
+/*   Updated: 2024/05/15 16:09:52 by maiboyer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MELLOC_INTERAL_H
+#define MELLOC_INTERAL_H
+
+#include "aq/allocator.h"
+#include "me/types.h"
+
+#define PAGE_LIST_MAX 255
+
+typedef struct s_page
+{
+	t_usize size;
+	void   *data;
+} t_page;
+
+typedef struct s_page_list
+{
+	t_usize				len;
+	t_page				pages[PAGE_LIST_MAX];
+	struct s_page_list *next;
+} t_page_list;
+
+struct s_allocator_melloc
+{
+	t_allocator_alloc		  alloc;
+	t_allocator_alloc_array	  alloc_array;
+	t_allocator_realloc		  realloc;
+	t_allocator_realloc_array realloc_array;
+	t_allocator_free		  free;
+	t_allocator_uninit		  uninit;
+	t_page_list				 *list;
+};
+
+t_error alloc_page_list(t_page_list **out);
+
+void *m_malloc(struct s_allocator_melloc *self, t_usize size);
+void *m_alloc_array(struct s_allocator_melloc *self, t_usize size,
+					t_usize count);
+void *m_realloc(struct s_allocator_melloc *self, void *ptr, t_usize min_size);
+void *m_realloc_array(struct s_allocator_melloc *self, void *ptr, t_usize size,
+					  t_usize count);
+void  m_free(struct s_allocator_melloc *self, void *ptr);
+void  m_uninit(struct s_allocator_melloc *self);
+
+#endif /* MELLOC_INTERAL_H */
--- allocator/src/me_alloc/functions1.c
+++ allocator/src/me_alloc/functions1.c
@@ -6,47 +6,109 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:02:12 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/05/14 18:21:16 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/05/15 16:18:19 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "aq/allocator.h"
-#include "aq/libc_wrapper.h"
+#include "aq/internal_vg_funcs.h"
+#include "aq/melloc_interal.h"
 #include "me/types.h"
 
-// void *__libc_malloc(t_usize size);
-// void *__libc_calloc(t_usize size, t_usize elem);
-// void *__libc_realloc(void *ptr, t_usize size);
-// void *__libc_realloc_array(void *ptr, t_usize size, t_usize elem);
-// void  __libc_free(void *ptr);
-//
-// void *lc_malloc(t_allocator *self, t_usize size)
-// {
-// 	(void)(self);
-// 	return (__libc_malloc(size));
-// }
-//
-// void *lc_calloc(t_allocator *self, t_usize size, t_usize elem)
-// {
-// 	(void)(self);
-// 	return (__libc_calloc(size, elem));
-// }
-//
-// void *lc_realloc(t_allocator *self, void *ptr, t_usize size)
-// {
-// 	(void)(self);
-// 	return (__libc_realloc(ptr, size));
-// }
-//
-// void *lc_realloc_array(t_allocator *self, void *ptr, t_usize size, t_usize elem)
-// {
-// 	(void)(self);
-// 	return (__libc_realloc_array(ptr, size, elem));
-// }
-//
-// void lc_free(t_allocator *self, void *ptr)
-// {
-// 	(void)(self);
-// 	return (__libc_free(ptr));
-// }
+void *__libc_malloc(t_usize size);
+void *__libc_calloc(t_usize size, t_usize elem);
+void *__libc_realloc(void *ptr, t_usize size);
+void *__libc_realloc_array(void *ptr, t_usize size, t_usize elem);
+void  __libc_free(void *ptr);
 
+t_usize round_to_pow2(t_usize val, t_usize pow)
+{
+	pow = (1 << pow) - 1;
+
+	return ((val + pow) & (~pow));
+}
+
+t_error alloc_page_list(t_page_list **out)
+{
+	t_page_list *val;
+
+	if (out == NULL)
+		return (ERROR);
+	val = __libc_calloc(sizeof(*val), 1);
+	if (val == NULL)
+		return (ERROR);
+	*out = val;
+	vg_mem_no_access(val, sizeof(*val));
+	vg_mempool_create_ext(val, 0);
+	return (NO_ERROR);
+}
+
+t_error alloc_new_page(struct s_allocator_melloc *alloc, t_usize page_size)
+{
+	t_page_list *list;
+
+	if (alloc->list == NULL && alloc_page_list(&alloc->list))
+		return (ERROR);
+	list = alloc->list;
+	while (list)
+	{
+		if (list->len != PAGE_LIST_MAX)
+			break;
+		if (list->next == NULL && alloc_page_list(&list->next))
+			return (ERROR);
+		list = list->next;
+	}
+	if (list == NULL)
+		return (ERROR);
+	list->pages[list->len].data = __libc_calloc(page_size, 1);
+	if (list->pages[list->len].data)
+		return (ERROR);
+	list->pages[list->len].size = page_size;
+	list->len++;
+	return (NO_ERROR);
+}
+
+void *m_malloc(struct s_allocator_melloc *self, t_usize size)
+{
+	(void)(self);
+	(void)(size);
+	return (NULL);
+}
+
+void *m_alloc_array(struct s_allocator_melloc *self, t_usize size,
+					t_usize count)
+{
+	(void)(self);
+	(void)(size);
+	(void)(count);
+	return (NULL);
+}
+
+void *m_realloc(struct s_allocator_melloc *self, void *ptr, t_usize min_size)
+{
+	(void)(self);
+	(void)(ptr);
+	(void)(min_size);
+	return (NULL);
+}
+
+void *m_realloc_array(struct s_allocator_melloc *self, void *ptr, t_usize size,
+					  t_usize count)
+{
+	(void)(self);
+	(void)(ptr);
+	(void)(size);
+	(void)(count);
+	return (NULL);
+}
+
+void m_free(struct s_allocator_melloc *self, void *ptr)
+{
+	(void)(self);
+	(void)(ptr);
+}
+
+void m_uninit(struct s_allocator_melloc *self)
+{
+	(void)(self);
+}
