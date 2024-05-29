1960bbc47a4d9823908aee3b07c08e5395f8106b
EniumRaphael <rparodi@student.42.fr>
2024-05-29 16:24:37 +0200

    Adding the negative number

--- math/main.c
+++ math/main.c
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 14:41:26 by rparodi           #+#    #+#             */
-/*   Updated: 2024/05/28 15:48:40 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/05/29 16:16:28 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 int main(int argc, char *argv[])
 {
-	t_number nb;
+	t_number *nb;
 
+	nb = (t_number *)malloc(sizeof(t_number));
 	if (argc != 2)
 	{
 		printf("Usage: %s <string>\n", argv[0]);
@@ -24,13 +25,13 @@ int main(int argc, char *argv[])
 	}
 	else if (argc == 2)
 	{
-		if (ft_init_numbers(argv[1], &nb) == ERROR)
+		if (ft_init_numbers(argv[1], nb) == ERROR)
 		{
 			printf("Error\n");
 			return 1;
 		}
 		else
-			printf("Number = %s \n(int: %s, %zu) (float: %s, %zu)\n", nb.number, nb.int_part, nb.int_size, nb.float_part, nb.float_size);
+			printf("Number = %s \n(int: %s, %zu) (float: %s, %zu)\n", nb->number, nb->int_part, nb->int_size, nb->float_part, nb->float_size);
 	}
 	return (0);
 }
--- math/sources/utils/ft_init_numbers.c
+++ math/sources/utils/ft_init_numbers.c
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:41:03 by rparodi           #+#    #+#             */
-/*   Updated: 2024/05/28 15:44:24 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/05/29 16:23:35 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,23 @@ t_error	ft_init_numbers(t_str str, t_number *nb)
 {
 	t_usize int_size;
 	t_usize	float_size;
+	t_u8	sign;
 	t_str	*tmp;
 
 	int_size = 0;
 	float_size = 0;
+	sign = 1;
 	if (nb == NULL)
 		return (ERROR);
-	if (ft_nblen(str, &int_size, &float_size) == ERROR)
+	if (ft_nblen(str, &int_size, &float_size, &sign) == ERROR)
 		return (ERROR);
 	else
 	{
-		nb = (t_number *)malloc(sizeof(t_number));
 		nb->number = str;
 		tmp = ft_split(str, '.');
+		nb->sign = sign;
 		nb->int_part = tmp[0];
 		nb->float_part = tmp[1];
-		// free_strs(tmp);
 		nb->int_size = int_size;
 		nb->float_size = float_size;
 	}
