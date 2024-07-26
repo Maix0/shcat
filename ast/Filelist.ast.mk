21b88bb4d91286c2f715b6dc9754af24bd63c4a0
Maieul BOYER <maieul.dev@familleboyer.net>
2024-07-26 14:50:05 +0200

    Filelist update

--- ast/Filelist.ast.mk
+++ ast/Filelist.ast.mk
@@ -2,7 +2,6 @@ SRC_FILES =                                                                   \
 from_node                                                                     \
 not_done_function                                                             \
 not_done_print                                                                \
-print_ast                                                                     \
 print_ast/ast_print_command                                                   \
 print_ast/ast_print_global                                                    \
 print_ast/ast_print_node                                                      \
