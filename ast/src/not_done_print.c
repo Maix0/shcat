0e0fb78ad2f504200af46a861f20d51149915e93
Raphaël <day_pinnate_0u@icloud.com>
2024-07-26 13:00:00 +0200

    adding files

--- ast/src/not_done_print.c
+++ ast/src/not_done_print.c
@@ -6,8 +6,19 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 11:00:25 by rparodi           #+#    #+#             */
-/*   Updated: 2024/07/26 11:33:01 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/07/26 12:58:54 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/function_declaration.h"
+
+void	ast_print_node_if(t_ast_node self) NOT_DONE;
+void	ast_print_node_case(t_ast_node self) NOT_DONE;
+void	ast_print_node_case_item(t_ast_node self) NOT_DONE;
+void	ast_print_node_elif(t_ast_node self) NOT_DONE;
+void	ast_print_node_else(t_ast_node self) NOT_DONE;
+void	ast_print_node_for(t_ast_node self) NOT_DONE;
+void	ast_print_node_until(t_ast_node self) NOT_DONE;
+void	ast_print_node_while(t_ast_node self) NOT_DONE;
+void	ast_print_node_heredoc_redirection(t_ast_node self) NOT_DONE;
 
