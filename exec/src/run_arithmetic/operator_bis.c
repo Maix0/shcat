2247d3662e444aac58bcac52ff3d8ebcffd8380d
Raphaël <day_pinnate_0u@icloud.com>
2024-09-02 19:28:06 +0200

    Corrected the norm on operator (exec/run_arith)

--- exec/src/run_arithmetic/operator_bis.c
+++ exec/src/run_arithmetic/operator_bis.c
@@ -6,15 +6,14 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:45:46 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/09/02 13:50:59 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/09/02 19:27:31 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec/_run_arith.h"
 #include "me/types.h"
 
-
-t_error		_postfix_op_dec(t_ast_node self, t_state *state, t_i64 *out)
+t_error	_postfix_op_dec(t_ast_node self, t_state *state, t_i64 *out)
 {
 	(void)(self);
 	(void)(state);
@@ -22,7 +21,7 @@ t_error		_postfix_op_dec(t_ast_node self, t_state *state, t_i64 *out)
 	return (ERROR);
 }
 
-t_error		_postfix_op_inc(t_ast_node self, t_state *state, t_i64 *out)
+t_error	_postfix_op_inc(t_ast_node self, t_state *state, t_i64 *out)
 {
 	(void)(self);
 	(void)(state);
@@ -30,9 +29,9 @@ t_error		_postfix_op_inc(t_ast_node self, t_state *state, t_i64 *out)
 	return (ERROR);
 }
 
-t_error		_unary_op_minus(t_ast_node self, t_state *state, t_i64 *out)
+t_error	_unary_op_minus(t_ast_node self, t_state *state, t_i64 *out)
 {
-	t_i64 val;
+	t_i64	val;
 
 	(void)(self);
 	(void)(state);
@@ -44,7 +43,7 @@ t_error		_unary_op_minus(t_ast_node self, t_state *state, t_i64 *out)
 	return (NO_ERROR);
 }
 
-t_error		_unary_op_plus(t_ast_node self, t_state *state, t_i64 *out)
+t_error	_unary_op_plus(t_ast_node self, t_state *state, t_i64 *out)
 {
 	return (_get_node_number(self, state, out));
 }
