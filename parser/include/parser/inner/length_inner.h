e4a11d298ec0ab760b3074d6a49b9b37af8be41b
Raphael <rparodi@student.42.fr>
2024-09-19 19:35:08 +0200

    style: noming lenght_inner (parser)

--- parser/include/parser/inner/length_inner.h
+++ parser/include/parser/inner/length_inner.h
@@ -6,22 +6,22 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 20:03:42 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/09/19 16:35:42 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/09/19 19:34:50 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LENGTH_INNER_H
-#define LENGTH_INNER_H
+# define LENGTH_INNER_H
 
-#include "me/types.h"
-#include "parser/inner/point_inner.h"
+# include "me/types.h"
+# include "parser/inner/point_inner.h"
 
-typedef struct s_length t_length;
+typedef struct s_length	t_length;
 
 struct s_length
 {
 	t_u32	bytes;
-	t_point extent;
+	t_point	extent;
 };
 
 t_length	length_add(t_length len1, t_length len2);
@@ -29,7 +29,6 @@ t_length	length_min(t_length len1, t_length len2);
 t_length	length_saturating_sub(t_length len1, t_length len2);
 t_length	length_sub(t_length len1, t_length len2);
 t_length	length_zero(void);
-bool length_is_undefined(t_length length);
-
+bool		length_is_undefined(t_length length);
 
 #endif /* LENGTH_INNER_H */
