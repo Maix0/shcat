1ab27a6205a32617d8cf5702aede43af73c8242e
Raphael <rparodi@student.42.fr>
2024-09-19 15:22:23 +0200

    style: noming env_clear

--- output/src/hashmap/env/env_clear.c
+++ output/src/hashmap/env/env_clear.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hashmap_env.h                              :+:      :+:    :+:   */
+/*   env_clear.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 11:00:22 by maiboyer          #+#    #+#             */
-/*   Updated: 2023/12/11 15:24:44 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/09/19 15:22:09 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "me/types.h"
-
-
-
 #include "me/hashmap/hashmap_env.h"
 #include "me/mem/mem.h"
 
-
 void	hmap_env_clear(t_hashmap_env *self)
 {
-	t_usize					bucket_id;
+	t_usize			bucket_id;
 	t_entry_env		*cur;
 	t_entry_env		*next;
 
