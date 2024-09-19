978636b6ef669b2278648bb6db183f392cd5adf4
Raphael <rparodi@student.42.fr>
2024-09-19 15:38:09 +0200

    style: noming ast_sort.c

--- output/src/vec/ast/ast_sort.c
+++ output/src/vec/ast/ast_sort.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   best_move.c                                        :+:      :+:    :+:   */
+/*   ast_sort.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 20:04:33 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/01/31 14:25:00 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/09/19 15:37:59 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "me/types.h"
 #include "me/vec/vec_ast.h"
 
-void vec_ast_sort(t_vec_ast		   *v,
-						  t_vec_ast_sort_fn is_sorted_fn)
+void	vec_ast_sort(t_vec_ast *v, t_vec_ast_sort_fn is_sorted_fn)
 {
-	t_usize		  sorted_part;
-	t_usize		  i;
-	t_ast_node tmp;
+	t_usize		sorted_part;
+	t_usize		i;
+	t_ast_node	tmp;
 
 	if (v == NULL)
-		return;
+		return ;
 	sorted_part = v->len;
 	while (sorted_part > 0)
 	{
