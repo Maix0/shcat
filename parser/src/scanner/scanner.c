dd87d203cdc25f60d41e49b5e0d312182b5d986e
Raphael <rparodi@student.42.fr>
2024-09-19 23:52:10 +0200

    style: noming scanner.c (scanner)

--- parser/src/scanner/scanner.c
+++ parser/src/scanner/scanner.c
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:41:11 by rparodi           #+#    #+#             */
-/*   Updated: 2024/09/15 20:22:37 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/09/19 23:51:57 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ bool	advance_word(t_lexer *lexer, t_string *unquoted_word);
 t_u32	serialize(t_scanner *scanner, t_u8 *buffer);
 void	deserialize(t_scanner *scanner, const t_u8 *buffer, t_u32 length);
 bool	scan_bare_dollar(t_lexer *lexer);
-bool	scan_double_hash(t_scanner *scanner, t_lexer *lexer,
+bool	scan_double_hash(t_scanner *scanner, t_lexer *lexer, \
 		const bool *valid_symbols);
-bool	scan_concat(t_scanner *scanner, t_lexer *lexer,
+bool	scan_concat(t_scanner *scanner, t_lexer *lexer, \
 		const bool *valid_symbols);
-bool	scan_heredoc_end(t_scanner *scanner, t_lexer *lexer,
+bool	scan_heredoc_end(t_scanner *scanner, t_lexer *lexer, \
 		const bool *valid_symbols);
-bool	scan_advance_words(t_scanner *scanner, t_lexer *lexer,
+bool	scan_advance_words(t_scanner *scanner, t_lexer *lexer, \
 		const bool *valid_symbols);
-bool	scan_literals(t_scanner *scanner, t_lexer *lexer,
+bool	scan_literals(t_scanner *scanner, t_lexer *lexer, \
 		const bool *valid_symbols);
 bool	scan(t_scanner *scanner, t_lexer *lexer, const bool *valid_symbols);
 void	*tree_sitter_sh_external_scanner_create(void);
-bool	tree_sitter_sh_external_scanner_scan(void *payload, t_lexer *lexer,
+bool	tree_sitter_sh_external_scanner_scan(void *payload, t_lexer *lexer, \
 		const bool *valid_symbols);
 t_u32	tree_sitter_sh_external_scanner_serialize(void *payload, t_u8 *state);
-void	tree_sitter_sh_external_scanner_deserialize(void *payload,
+void	tree_sitter_sh_external_scanner_deserialize(void *payload, \
 		const t_u8 *state, t_u32 length);
 void	tree_sitter_sh_external_scanner_destroy(void *payload);
 
@@ -199,7 +199,8 @@ bool	scan_literals(t_scanner *scanner, t_lexer *lexer,
 			|| lexer->funcs.lookahead == '%' || lexer->funcs.lookahead == '#'
 			|| lexer->funcs.lookahead == '/')
 			return (false);
-		if (valid_symbols[EXTGLOB_PATTERN] && me_isspace(lexer->funcs.lookahead))
+		if (valid_symbols[EXTGLOB_PATTERN] && \
+			me_isspace(lexer->funcs.lookahead))
 		{
 			lexer->funcs.mark_end((void *)lexer);
 			lexer->funcs.result_symbol = EXTGLOB_PATTERN;
@@ -209,7 +210,8 @@ bool	scan_literals(t_scanner *scanner, t_lexer *lexer,
 	is_number = true;
 	if (me_isdigit(lexer->funcs.lookahead))
 		lexer->funcs.advance((void *)lexer, false);
-	else if (me_isalpha(lexer->funcs.lookahead) || lexer->funcs.lookahead == '_')
+	else if (me_isalpha(lexer->funcs.lookahead) || \
+		lexer->funcs.lookahead == '_')
 	{
 		is_number = false;
 		lexer->funcs.advance((void *)lexer, false);
@@ -274,12 +276,12 @@ bool	scan_literals(t_scanner *scanner, t_lexer *lexer,
 bool	scan(t_scanner *scanner, t_lexer *lexer, const bool *valid_symbols)
 {
 	if (valid_symbols[CONCAT] && !(valid_symbols[ERROR_RECOVERY])
-		&& (!(lexer->funcs.lookahead == 0 || me_isspace(lexer->funcs.lookahead)
-				|| lexer->funcs.lookahead == '>' || lexer->funcs.lookahead == '<'
-				|| lexer->funcs.lookahead == ')' || lexer->funcs.lookahead == '('
-				|| lexer->funcs.lookahead == ';' || lexer->funcs.lookahead == '&'
-				|| lexer->funcs.lookahead == '|' || lexer->funcs.lookahead == '{'
-				|| lexer->funcs.lookahead == '}')))
+		&& (!(lexer->funcs.lookahead == 0 || me_isspace(lexer->funcs.lookahead) \
+			|| lexer->funcs.lookahead == '>' || lexer->funcs.lookahead == '<' \
+			|| lexer->funcs.lookahead == ')' || lexer->funcs.lookahead == '(' \
+			|| lexer->funcs.lookahead == ';' || lexer->funcs.lookahead == '&' \
+			|| lexer->funcs.lookahead == '|' || lexer->funcs.lookahead == '{' \
+			|| lexer->funcs.lookahead == '}')))
 		return (scan_concat(scanner, lexer, valid_symbols));
 	if (scan_double_hash(scanner, lexer, valid_symbols))
 		return (true);
