9f55be8a7c7db263ba016ef97dd8715463900b1a
Raphael <rparodi@student.42.fr>
2024-09-20 13:29:51 +0200

    fix: fixed the script kiddy

--- parser/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-clang -Wno-unused-command-line-argument -Xclang -ast-dump=json ./lib.c -fsyntax-only -ferror-limit=99999 |
-	jq '.inner[] | select(.name) | select((.name | startswith("_"))| not)' |
-	jq 'select((.kind == "VarDecl" or .kind == "FunctionDecl") | not)' |
-	jq 'walk(if type == "object" then del(.loc, .id, .range, .typeAliasDeclId, .isReferenced) else . end)' |
-	jq 'select(.type?.qualType // .name // "" | startswith("_") | not) | select(.name // "" | endswith("_t") | not )' |
-	jq -n '[inputs]'
-	
