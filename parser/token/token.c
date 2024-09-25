659785cc44c2d619028f3fb26540dac037c03265
Raphaël <day_pinnate_0u@icloud.com>
2024-09-25 16:58:54 +0200

    fix: patching the not compilation of this file [token.c (main file of tokeneiser)]

--- parser/token/token.c
+++ parser/token/token.c
@@ -6,17 +6,32 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:27:03 by rparodi           #+#    #+#             */
-/*   Updated: 2024/09/25 16:54:08 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/09/25 16:58:17 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser/token/token.h"
 #include "me/string/string.h"
 #include "me/types.h"
+#include <stdio.h>
 
-t_error	handle_quote(t_string raw, enum e_token actual, int *output);
-t_error	handle_arithmetic(t_string raw, enum e_token actual, int *output);
+t_error	handle_quote(t_string raw, enum e_token actual, int *output)
+{
+	(void)raw;
+	(void)actual;
+	(void)output;
+	printf("\tNOT DONE\n");
+	return (ERROR);
+}
 
+t_error	handle_arithmetic(t_string raw, enum e_token actual, int *output)
+{
+	(void)raw;
+	(void)actual;
+	(void)output;
+	printf("\tNOT DONE\n");
+	return (ERROR);
+}
 t_error	tokeniser(t_string raw, enum e_token actual, int *output)
 {
 	if (!raw.buf || raw.len == 0 || raw.capacity == 0)
