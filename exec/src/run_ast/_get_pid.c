8242d8eaaf43ef698b7a79097a56582046f37541
maix0 <maieul.dev@familleboyer.net>
2024-10-06 14:42:05 +0200

    update: now it builds correctly

--- exec/src/run_ast/_get_pid.c
+++ exec/src/run_ast/_get_pid.c
@@ -6,14 +6,12 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 14:25:20 by maiboyer          #+#    #+#             */
-/*   Updated: 2024/10/06 14:27:40 by maiboyer         ###   ########.fr       */
+/*   Updated: 2024/10/06 14:41:51 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "me/os/os.h"
 #include "me/types.h"
-
-#define BONUS 1
 
 #if BONUS
 t_pid get_self_pid(void)
